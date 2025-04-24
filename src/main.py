11bdfe5a130b41e058ac67cafc9c766e7c89dcd0
juzdalua <bearwcw@naver.com>
2025-04-24 16:58:36 +0900

    Fix: reconnect twice

--- src/main.py
+++ src/main.py
@@ -1,20 +1,13 @@
+import threading
 from dotenv import load_dotenv
-import os
-import time
-
-from utils.socket import connect_to_server, ping, send_loop, send_message, disconnect_from_server
+from utils.socket import keep_reconnecting
 
 load_dotenv()
-server_url = "http://localhost:6001"
-connectFlag = False
 
-try:
-    while True:
-      if connectFlag == False:
-        connectFlag = connect_to_server(server_url)
-      else:
-         pass
-except Exception as e:
-    print(f"연결 실패: {e}")
+t = threading.Thread(target=keep_reconnecting)
+t.daemon = True
+t.start()
 
-# send_loop()
+
+
+t.join()
--- src/utils/socket.py
+++ src/utils/socket.py
@@ -3,58 +3,49 @@ import time
 
 sio = socketio.Client()
 connectFlag = False
+server_url = "http://localhost:6001"
 
 @sio.event
 def connect():
-    global connectFlag
-    connectFlag = True
-    register()
-    print("Connected ✅")
+  global connectFlag
+  connectFlag = True
+  register()
+  print("Connected ✅")
 
 @sio.event
 def ping(data):
-    print(f"[RECV]: {data}")
-    sio.emit('pong','pong')
-    print('[SEND] pong')
+  print(f"[RECV]: {data}")
+  sio.emit('pong','pong')
+  print('[SEND] pong')
 
 @sio.event
 def disconnect():
-    global connectFlag
-    connectFlag = False
-    print("Disconnected ❌")
+  global connectFlag
+  connectFlag = False
+  print("Disconnected ❌")
 
 def send_message(eventKey: str, message: str):
-    sio.emit(eventKey, message)
-
-def send_loop():
-    global connectFlag
-    try:
-      while connectFlag == True:
-          send_message("message", "안녕하세요, 서버!")
-          time.sleep(1)
-    except KeyboardInterrupt:
-        disconnect_from_server()
-        connectFlag = False
-        print("Close socket")
+  sio.emit(eventKey, message)
 
 def connect_to_server(url: str):
-    try:
-        sio.connect(url)
-        print(f"{url}에 연결되었습니다.")
-        return True
-    except Exception as e:
-        print(f"연결 실패: {e}")
-        return False
+  try:
+    sio.connect(url)
+    print(f"{url}에 연결되었습니다.")
+    return True
+  except Exception as e:
+    print(f"연결 실패: {e}")
+    return False
 
 def register():
-    sio.emit('register', {'clientType': 'etc'})
+  sio.emit('register', {'clientType': 'etc'})
 
-def disconnect_from_server():
-    sio.disconnect()
-    print("서버와 연결이 종료되었습니다.")
-
-def keep_reconnecting(url: str):
-    while not connectFlag:
+def keep_reconnecting():
+    global connectFlag, server_url
+    while True:
+      if not connectFlag:
         print("Reconnecting...")
-        time.sleep(5)
-        connectFlag = connect_to_server(url)
+        try:
+          connectFlag = connect_to_server(server_url)
+        except Exception as e:
+            print(f"[❌ 재연결 실패]: {e}")
+      time.sleep(2)
